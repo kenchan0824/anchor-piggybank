139f0c4312520adc1581a4b1c5ba66464eda5e98
Kendrick Chan <kenchan0824@gmail.com>
2024-11-07 19:45:30 +0800

    project init

---
+++ programs/piggybank/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("8izb58TQydRmNiWCygaQBrVeRgh1XUNPxZjkbWqr8dLj");
+
+#[program]
+pub mod piggybank {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
